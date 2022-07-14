e332277622906a125b516a55551b54136fa191cf
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-14 19:16:33 +0200

    lab terminado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/header"
+import Card from "./components/card";
+
 import './App.css';
 
 
@@ -7,11 +9,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className = "header">
-      <Navbar />  
-      <Header />
 
+      <div className="header">
+        <Navbar />
+        <Header />
       </div>
+
+      <div className="cardslist">
+        <Card />
+       
+      </div>
+
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,66 @@
 function Card() {
     return (
-        <div className="card">
-
+        <>
+        <div className= "card">
+            
             <img
                 id="card1"
-                src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+                // src=".././icon1.png"
+                src= "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
                 alt="icon 1"
             />
-            <h4 className = "card-title">Declarative</h4>
-            <p className = "card-text"> React make it painñess to create interactive UIs.</p>
-
+            <div className = "text-card">
+            <h4>Declarative</h4>
+            <p className = "card-text"> React make it painless to create interactive UIs.</p>
+            </div>
+            
         </div>
+                <div className= "card">
+            
+                <img
+                    id="card2"
+                    // src=".././icon1.png"
+                    src= "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+                    alt="icon 1"
+                />
+                <div className = "text-card">
+                <h4>Components</h4>
+                <p className = "card-text"> Build encapapsulated components that manage their state.</p>
+                </div>
+                
+            </div>
+
+            <div className= "card">
+            
+            <img
+                id="card3"
+                // src=".././icon1.png"
+                src= "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+                alt="icon 1"
+            />
+            <div className = "text-card">
+            <h4>Single-Way</h4>
+            <p className = "card-text">A set of immutable values are passed to the component's.</p>
+            </div>
+            
+        </div>
+
+        <div className= "card">
+            
+            <img
+                id="card4"
+                // src=".././icon1.png"
+                src= "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+                alt="icon 1"
+            />
+            <div className = "text-card">
+            <h4>JSX</h4>
+            <p className = "card-text">Statically-typed, designed to run on modern browsers.</p>
+            </div>
+            
+        </div>
+            </>
+        
     )
 
 }
